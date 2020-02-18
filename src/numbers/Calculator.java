111f5cf3a097e860b400cbc61d0fb2a87c1d4a74
Ashley Wilson <ashleybwilson11@gmail.com>
2020-02-17 21:51:01 -0600

    add files

---
+++ src/numbers/Calculator.java
@@ -0,0 +1,26 @@
+package numbers;
+
+import javax.swing.*;
+
+public class Calculator {
+    private JPanel panel1;
+    private JButton divideButton;
+    private JButton enterButton;
+    private JButton signButton;
+    private JButton percentButton;
+    private JButton sevenButton;
+    private JButton eightButton;
+    private JButton nineButton;
+    private JButton fourButton;
+    private JButton fiveButton;
+    private JButton clearButton;
+    private JButton multiplyButton;
+    private JButton sixButton;
+    private JButton oneButton;
+    private JButton twoButton;
+    private JButton threeButton;
+    private JButton subtractButton;
+    private JButton addButton;
+    private JButton zeroButton;
+    private JButton decimalButton;
+}
