0b934fb316a25e4bb084203037700f5e020f9f31
Ashley Wilson <ashleybwilson11@gmail.com>
2020-02-17 22:01:47 -0600

    Finished buttons

--- src/numbers/Calculator.java
+++ src/numbers/Calculator.java
@@ -3,7 +3,7 @@ package numbers;
 import javax.swing.*;
 
 public class Calculator {
-    private JPanel panel1;
+    private JPanel CalculatorView;
     private JButton divideButton;
     private JButton enterButton;
     private JButton signButton;
@@ -23,4 +23,13 @@ public class Calculator {
     private JButton addButton;
     private JButton zeroButton;
     private JButton decimalButton;
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Calculator");
+        frame.setContentPane(new Calculator().CalculatorView);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
 }
